cb5b5efd873d8aeea65fffc587763a2c8430d542
Harsh_Pachauri <harshpachauri205@gmail.com>
2024-09-03 23:24:32 +0530

    Connected to mongodb

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app=express()
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MongoDB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR:",error);
+//             throw error
+//         })
+//     } catch (error) {
+//         console.log("ERROR:",error)
+//         throw error
+//     }
+// })()
